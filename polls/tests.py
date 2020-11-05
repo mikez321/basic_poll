2e2058016fe320fa493984285e39ef659719f679
Mike Hernandez <hernandezmichael321@gmail.com>
2020-11-04 20:17:52 -0700

    Add more tests for was_published_recently method

--- polls/tests.py
+++ polls/tests.py
@@ -12,3 +12,15 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_old_questions(self):
+        """Questions older than one day are old."""
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_recent_questions(self):
+        """Questions posted in the last 24 hours return as True."""
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
